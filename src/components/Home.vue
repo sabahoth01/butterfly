a258007d7883926c6decb69b3a6425059ef3da4a
Brunnotte Wasaulua <bt.wasaulua@gmail.com>
2022-08-15 16:24:22 +0300

    final fix errors for responsive design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -112,7 +112,7 @@
     <div class="quotes row reveal my-md-5 m-1 p-md-4 p-1 d-flex flex-nowrap flex-md-row flex-column g-2">
       <div class="quotes-content  col-md-3 col-12 ">
         <img :src="doing" alt="citation-image" class="">
-        <blockquote class="p-md-2 p-1 bg-black bg-opacity-50 top-0 position-absolute h-100 w-100">
+        <blockquote class="p-md-2 px-1 bg-black bg-opacity-50 top-0 position-absolute h-100 w-100">
           <p class="body-text quote-text position-relative">
             La meilleure manière de commencer est d’arrêter de discuter et de commencer à faire.<br/>
             &mdash; <cite>WALT DISNEY</cite>
@@ -121,7 +121,7 @@
       </div>
       <div class="quotes-content col-md-3 col-12">
         <img :src="choice" alt="citation-image">
-        <blockquote class=" p-md-2 p-1 bg-black bg-opacity-50  top-0 position-absolute h-100 w-100 ">
+        <blockquote class=" p-md-2 px-1 bg-black bg-opacity-50  top-0 position-absolute h-100 w-100 ">
           <p class="body-text quote-text position-relative">
             Ce sont nos choix, Harry, qui montrent ce que nous sommes vraiment, bien plus que nos capacités.<br/>
             &mdash; <cite>J.K. ROWLING</cite>
@@ -130,7 +130,7 @@
       </div>
       <div class="quotes-content col-md-3 col-12">
         <img :src="smile" alt="citation-image">
-        <blockquote class=" p-md-2 p-1 bg-black bg-opacity-50 top-0 position-absolute h-100 w-100">
+        <blockquote class=" p-md-2 px-1 bg-black bg-opacity-50 top-0 position-absolute h-100 w-100">
           <p class="body-text quote-text position-relative">
             Ne pleurez pas parce que c’est fini, souriez parce que c’est arrivé.<br/>
             &mdash; <cite>Dr. SEUSS</cite>
@@ -413,6 +413,7 @@ button:hover {
   }
   .quote-text{
     font-size: 13px;
+    padding: 0.2%;
   }
 
 }
